e75bb2dfa07b283c9eaf920b9436208cb2400605
Samantha Au <samanthaau@live.com>
2022-04-08 14:27:27 +1000

    added box shadow below navbar transition element and adjusted padding for elements

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ $(".navTrigger").click(function () {
   $("#mainListDiv").toggleClass("show_list");
   $("#mainListDiv").fadeIn();
 });
+
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 50) {
+    $(".nav").addClass("affix");
+    console.log("OK");
+  } else {
+    $(".nav").removeClass("affix");
+  }
+});
