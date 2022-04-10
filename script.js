2767d16d6321ba31f5ffd78f2842192f577dff1f
Samantha Au <samanthaau@live.com>
2022-04-10 21:50:56 +1000

    added more technology icons to the about me section

--- script.js
+++ script.js
@@ -31,3 +31,26 @@ $(window).scroll(function () {
     $(".nav").removeClass("affix");
   }
 });
+
+var messages = ["message1", "message2  message2 message2", "message3 message3"];
+var rank = 0;
+
+// Code for Chrome, Safari and Opera
+document
+  .getElementById("typewriter")
+  .addEventListener("webkitAnimationEnd", changeTxt);
+
+function changeTxt(e) {
+  _h1 = this.getElementsByTagName("p")[0];
+  _h1.style.webkitAnimation = "none"; // set element animation to none
+  setTimeout(function () {
+    // you surely want a delay before the next message appears
+    _p.innerHTML = messages[rank];
+    var speed = (3.5 * messages[rank].length) / 20; // adjust the speed (3.5 is the original speed, 20 is the original string length
+    _p.style.webkitAnimation =
+      "typing " +
+      speed +
+      "s steps(40, end), blink-caret .75s step-end infinite"; //  switch to the original set of animation
+    rank === messages.length - 1 ? (rank = 0) : rank++; // if you have displayed the last message from the array, go back to the first one, else go to next message
+  }, 1000);
+}
