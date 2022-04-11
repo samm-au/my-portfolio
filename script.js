818106f9bb3196ee7ab9277241138a28b6e21c6b
Samantha Au <samanthaau@live.com>
2022-04-12 08:45:10 +1000

    updated class names and added beating heart animation

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-// $(".navTrigger").click(function () {
-//   $(this).toggleClass("active");
-//   console.log("Clicked menu");
-//   $("#mainListDiv").toggleClass("show_list");
-//   $("#mainListDiv").fadeIn();
-// });
-
-// $(window).scroll(function () {
-//   if ($(document).scrollTop() > 50) {
-//     $(".nav").addClass("affix");
-//     console.log("OK");
-//   } else {
-//     $(".nav").removeClass("affix");
-//   }
-// });
-
-// src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js";
-
-$(".navTrigger").click(function () {
-  $(this).toggleClass("active");
-  console.log("Clicked menu");
-  $("#mainListDiv").toggleClass("show_list");
-  $("#mainListDiv").fadeIn();
-});
-
-$(window).scroll(function () {
-  if ($(document).scrollTop() > 50) {
-    $(".nav").addClass("affix");
-    console.log("OK");
-  } else {
-    $(".nav").removeClass("affix");
-  }
-});
-
 var messages = ["message1", "message2  message2 message2", "message3 message3"];
 var rank = 0;
 
